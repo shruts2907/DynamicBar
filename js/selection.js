55e78cde7c62185fd53f2fd3e0ae103371b15826
Shrutika <shruts2907@gmail.com>
2013-06-14 21:06:37 -0300

    Create selection.js
    all the data is stored in array

---
+++ js/selection.js
@@ -0,0 +1,19 @@
+var select = new Object();
+
+select["begin"]=["1","Liquor","Soda","Water"];
+
+select["Liquor"]=["2","Wine","Beer","Other"];
+select["Soda"]=["2","Pepsico","Coca_Cola","Dr_Pepper"];
+select["Water"]=["2","Gatorade","Life_Water","Plain_Water"];
+
+select["Wine"]=["3","Red","White","Honey"];
+select["Beer"]=["3","Stout","St_Patrick_Day_Special","Pale_Ale"];
+select["Other"]=["3","Vodka","Martini","Tequila"];
+
+select["Pepsico"]=["3","Pepsi","Diet","Mt_Dew"];
+select["Coca_Cola"]=["3","Cola","Diet","Sprite"];
+select["Dr_Pepper"]=["3","Regular","Diet","Shandy"];
+
+select["Gatorade"]=["3","Orange","Red","Green"];
+select["Life_Water"]=["3","Orange","Red","Green"];
+select["Plain_Water"]=["3","Aquafina","Dasani","Tap_Water"];
